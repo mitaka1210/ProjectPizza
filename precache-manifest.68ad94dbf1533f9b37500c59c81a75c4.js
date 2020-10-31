1e4d9b8f69dc2f08c7f6151ebb7e3d5c6e56bdab
Dimitar <dimitar_dimitrov12@mail.bg>
2020-10-31 17:45:36 +0200

    Updates

---
+++ precache-manifest.68ad94dbf1533f9b37500c59c81a75c4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63253584377ba06b8f8bad656cf0d0a0",
+    "url": "/ProjectPizza/index.html"
+  },
+  {
+    "revision": "dd995414c401a7e07438",
+    "url": "/ProjectPizza/static/css/main.d692b922.chunk.css"
+  },
+  {
+    "revision": "6163b70ad35737be1dac",
+    "url": "/ProjectPizza/static/js/2.16c091dc.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/ProjectPizza/static/js/2.16c091dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd995414c401a7e07438",
+    "url": "/ProjectPizza/static/js/main.fb1abfcd.chunk.js"
+  },
+  {
+    "revision": "542d8ffc7f489fdc1fc9a1f0e7000026",
+    "url": "/ProjectPizza/static/js/main.fb1abfcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea46a462ba49fa4baac",
+    "url": "/ProjectPizza/static/js/runtime-main.f25fe5bf.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/ProjectPizza/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/ProjectPizza/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/ProjectPizza/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/ProjectPizza/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/ProjectPizza/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/ProjectPizza/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/ProjectPizza/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/ProjectPizza/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/ProjectPizza/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/ProjectPizza/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/ProjectPizza/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/ProjectPizza/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/ProjectPizza/static/media/empty-cart.ceb2faf0.png"
+  },
+  {
+    "revision": "2d5c7cb874064764bb5f9fa5cc93ddf3",
+    "url": "/ProjectPizza/static/media/payment-method.2d5c7cb8.svg"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/ProjectPizza/static/media/pizza-logo.f3327011.svg"
+  }
+]);
